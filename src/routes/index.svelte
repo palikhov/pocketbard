1a9a98c81ca5d8cd834fc2ab5ad4ea6b8f0730a5
Aleksandr Vasilenko <phenomen@pm.me>
2021-12-01 03:59:05 -0600

    Optimized stories loading

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,10 +7,10 @@
   }
 
   export async function load() {
-    const stories = await Promise.all(body);
+    const storyData = await Promise.all(body);
     return {
       props: {
-        storyData: stories,
+        storyData,
       },
     };
   }
@@ -19,10 +19,10 @@
 <script>
   import MdErrorOutline from "svelte-icons/md/MdErrorOutline.svelte";
   import GiCastle from "svelte-icons/gi/GiCastle.svelte";
-  import GiDwarfFace from "svelte-icons/gi/GiDwarfFace.svelte";
+  import GiWomanElfFace from "svelte-icons/gi/GiWomanElfFace.svelte";
   import GiSpikedDragonHead from "svelte-icons/gi/GiSpikedDragonHead.svelte";
   import GiMagicSwirl from "svelte-icons/gi/GiMagicSwirl.svelte";
-  import GiSwapBag from "svelte-icons/gi/GiSwapBag.svelte";
+  import GiLockedChest from "svelte-icons/gi/GiLockedChest.svelte";
 
   export let storyData;
 
@@ -53,53 +53,50 @@
 
 <div class="prose mb-10">
   <h1>Карманный Бард</h1>
-
-  <h4>Историй в репертуаре: {tagged.length}</h4>
 </div>
 
 <div class="items-center my-2">
   <button class="btn btn-success p-1" on:click={() => setTag("сцена")}><GiCastle /></button>
-  <button class="btn btn-error p-1" on:click={() => setTag("персонаж")}><GiDwarfFace /></button>
-  <button class="btn btn-primary p-1" on:click={() => setTag("существо")}><GiSpikedDragonHead /></button>
-  <button class="btn btn-secondary p-1" on:click={() => setTag("заклинание")}><GiMagicSwirl /></button>
-  <button class="btn btn-active p-1" on:click={() => setTag("предмет")}><GiSwapBag /></button>
+  <button class="btn btn-info p-1" on:click={() => setTag("персонаж")}><GiWomanElfFace /></button>
+  <button class="btn btn-error p-1" on:click={() => setTag("существо")}><GiSpikedDragonHead /></button>
+  <button class="btn btn-primary p-1" on:click={() => setTag("заклинание")}><GiMagicSwirl /></button>
+  <button class="btn btn-active p-1" on:click={() => setTag("предмет")}><GiLockedChest /></button>
 </div>
 
 <div class="form-control my-5">
   <div class="relative">
-    <input
-      type="text"
-      class="w-full lg:w-1/2 pr-16 input input-bordered"
-      placeholder="название, категория, тэг..."
-      bind:value={tagFilter}
-    />
+    <input type="text" class="w-full lg:w-1/2 pr-16 input" placeholder="название, категория, тэг..." bind:value={tagFilter} />
     <button class="absolute top-0 right-0 lg:right-1/2 rounded-l-none btn btn-ghost" on:click={() => setTag("")}>✕</button>
   </div>
 </div>
 
 <div class="grid grid-cols-1 lg:grid-cols-2 gap-2">
   {#each tagged as { title, slug, category, tags, author, description }}
-    <div class="card-body py-2 rounded-md bg-base-100">
+    <div class="p-4 rounded-md bg-base-100">
       <a sveltekit:prefetch href="/story/{slug}"
         ><h2
-          class="text-2xl card-title"
+          class="text-2xl hover:link"
           class:text-success={category == "сцена"}
-          class:text-error={category == "персонаж"}
-          class:text-primary={category == "существо"}
-          class:text-secondary={category == "заклинание"}
+          class:text-info={category == "персонаж"}
+          class:text-error={category == "существо"}
+          class:text-primary={category == "заклинание"}
           class:text-neutral-content={category == "предмет"}
         >
           {title}
         </h2>
       </a>
-      <div class="mb-4 space-x-1 card-actions">
+      <div class="my-2 space-x-1">
         {#each tags as tag}
-          <button class="badge rounded-md" class:text-success={tagFilter == tag} on:click={() => setTag(tag)}>{tag}</button>
+          <button class="badge hover:badge-accent rounded-md" class:badge-accent={tagFilter == tag} on:click={() => setTag(tag)}
+            >{tag}</button
+          >
         {/each}
       </div>
-      <p class="prose text-sm">
-        {description.substr(0, 120)}...
-      </p>
+      <a sveltekit:prefetch href="/story/{slug}">
+        <p class="prose text-sm">
+          {description.substr(0, 120)}...
+        </p>
+      </a>
     </div>
   {:else}
     <div class="alert">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,7 @@ const config = {
   plugins: [forms, typography, daisyui],
 
   daisyui: {
-    themes: ["dark"],
+    themes: false,
   },
 };
 
